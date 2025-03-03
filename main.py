29abe479533f9d57693e1335d15885de1b9c3ab6
Nadan parinda <sukalalthakare19@gmail.com>
2025-03-03 10:32:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import asyncio as aio
 from bot import *
-
-
-async def async_main():
-    db = DB()
-    await db.connect()
     
 if __name__ == '__main__':
     loop = aio.get_event_loop_policy().get_event_loop()
