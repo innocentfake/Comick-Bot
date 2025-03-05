c2a0381d5457fcbeee9c36014819727184d64d69
Nadan parinda <sukalalthakare19@gmail.com>
2025-03-05 16:36:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -391,12 +391,12 @@ async def process_chapter_queue(user_id):
             # 🟢 Set caption with manga title
             caption = f"<blockquote><b>[MS] [{chap_num}] {sanitized_title} @Manga_Sect</b></blockquote>"
 
-            thumbnail_path = thumb.jpg[0]
+            thumb_path = "thumb.jpg"
             await bot.send_document(
                 chat_id=callback_query.message.chat.id,
                 document=pdf_path,
                 caption=caption,
-                thumb=thumbnail_path
+                thumb=thumb_path
             )
 
             shutil.rmtree(download_dir)
