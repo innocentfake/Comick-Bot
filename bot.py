0aec60037890a0e32e99b8a669030945ffdfa866
Nadan parinda <sukalalthakare19@gmail.com>
2025-03-25 19:58:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from pyrogram.types import message 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.enums import ParseMode
 import cloudscraper
